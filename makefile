a00f3028601f8cec0f4d6de1a399ab3740395fb8
Ennaox <alexis.laplanche@hotmail.fr>
2021-12-24 19:14:47 +0100

    Ajout v3

--- makefile
+++ makefile
@@ -1,14 +1,36 @@
-all: basenbody.g v1enbody.g
+all: basenbody.g v1nbody.g v2nbody.g v3nbody256.g v3nbody512.g
 
-bench:
+bench: base v1 v2 v3_256 v3_512
+
+base: basenbody.g 
 	taskset -c 3 ./basenbody.g
-	taskset -c 3 ./v1enbody.g
+
+v1: v1nbody.g
+	taskset -c 3 ./v1nbody.g
+
+v2: v2nbody.g
+	taskset -c 3 ./v2nbody.g
+
+v3_256: v3nbody256.g
+	taskset -c 3 ./v3nbody256.g
+
+v3_512: v3nbody512.g
+	taskset -c 3 ./v3nbody512.g
 
 basenbody.g: basenbody.c
 	gcc -march=native -mavx2 -Ofast -fopt-info-all=nbody.gcc.optrpt $< -o $@ -lm -fopenmp
 
-v1enbody.g: v1nbody.c
+v1nbody.g: v1nbody.c
 	gcc -march=native -mavx2 -Ofast -fopt-info-all=nbody.gcc.optrpt $< -o $@ -lm -fopenmp
+
+v2nbody.g: v2nbody.c
+	gcc -march=native -mavx2 -Ofast -fopt-info-all=nbody.gcc.optrpt $< -o $@ -lm -fopenmp
+
+v3nbody256.g: v3nbody256.c
+	gcc -march=native -mavx2 -Ofast -fopt-info-all=nbody.gcc.optrpt $< -o $@ -lm -fopenmp -g
+
+v3nbody512.g: v3nbody512.c
+	gcc -march=native -mavx2 -Ofast -fopt-info-all=nbody.gcc.optrpt $< -o $@ -lm -fopenmp -g
 
 basenbody.i: basenbody.c
 	icc -xhost -Ofast -qopt-report $< -o $@ -qmkl -qopenmp
@@ -16,6 +38,16 @@ basenbody.i: basenbody.c
 v1nbody.i: v1nbody.c
 	icc -xhost -Ofast -qopt-report $< -o $@ -qmkl -qopenmp
 
+v2nbody.i: v2nbody.c
+	icc -xhost -Ofast -qopt-report $< -o $@ -qmkl -qopenmp
+
+v3nbody256.i: v3nbody256.c
+	icc -xhost -Ofast -qopt-report $< -o $@ -qmkl -qopenmp
+
+v3nbody512.i: v3nbody512.c
+	icc -xhost -Ofast -qopt-report $< -o $@ -qmkl -qopenmp
+
+
 clean:
-	rm -Rf *~ nbody.g nbody.i *.optrpt *.g
+	rm -f *~ *.g *.i *.optrpt
 
---
+++ v3nbody256.c
@@ -0,0 +1,201 @@
+//
+#include <omp.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <immintrin.h>
+//
+typedef float              f32;
+typedef double             f64;
+typedef unsigned long long u64;
+
+#define ALIGN 64
+
+//
+typedef struct particle_s {
+
+  f32 * restrict x,* restrict y,* restrict z;
+  f32 * restrict vx,* restrict vy,* restrict vz;
+  
+} particle_t;
+
+//
+void init(particle_t *p, u64 n)
+{
+  for (u64 i = 0; i < n; i++)
+    {
+      //
+      u64 r1 = (u64)rand();
+      u64 r2 = (u64)rand();
+      f32 sign = (r1 > r2) ? 1 : -1;
+      
+      //
+      p->x[i] = sign * (f32)rand() / (f32)RAND_MAX;
+      p->y[i] = (f32)rand() / (f32)RAND_MAX;
+      p->z[i] = sign * (f32)rand() / (f32)RAND_MAX;
+
+      //
+      p->vx[i] = (f32)rand() / (f32)RAND_MAX;
+      p->vy[i] = sign * (f32)rand() / (f32)RAND_MAX;
+      p->vz[i] = (f32)rand() / (f32)RAND_MAX;
+    }
+}
+
+//
+void move_particles(particle_t *p, const f32 dt, u64 n)
+{
+  //
+  const f32 softening = 1e-20;
+
+  __m256 vsoftening = _mm256_load_ps(&softening);
+
+  __m256 pxi, pyi, pzi, pxj, pyj, pzj, vxi, vyi, vzi;
+
+  __m256 vdt = _mm256_load_ps(&dt);
+
+  //
+  for (u64 i = 0; i < n; i++)
+    {
+      printf("%lu \n",i);
+      pxi = _mm256_load_ps(&p->x[i]);
+      pyi = _mm256_load_ps(&p->y[i]);
+      pzi = _mm256_load_ps(&p->z[i]);
+
+      vxi = _mm256_load_ps(&p->vx[i]);
+      vyi = _mm256_load_ps(&p->vy[i]);
+      vzi = _mm256_load_ps(&p->vz[i]);
+
+      //
+      __m256 vfx = _mm256_setzero_ps();
+      __m256 vfy = _mm256_setzero_ps();
+      __m256 vfz = _mm256_setzero_ps();
+      //23 floating-point operations
+    for (u64 j = 0; j < n; j+= 8)
+    {
+      pxj = _mm256_load_ps(&p->x[j]);
+      pyj = _mm256_load_ps(&p->y[j]);
+      pzj = _mm256_load_ps(&p->z[j]);
+      //Newton's law
+      const __m256 dx = _mm256_sub_ps(pxj,pxi); //1
+      const __m256 dy = _mm256_sub_ps(pyj,pyi); //2
+      const __m256 dz = _mm256_sub_ps(pzj,pzi); //3
+      const __m256 d_2 = _mm256_add_ps(_mm256_add_ps(_mm256_mul_ps(dx,dx),_mm256_mul_ps(dy,dy)),_mm256_add_ps(_mm256_mul_ps(dz,dz),vsoftening)); //9
+      __m256 tmp = _mm256_rsqrt_ps(d_2);  //10
+      const __m256 d_3_over_2 = _mm256_mul_ps(_mm256_mul_ps(tmp,tmp),tmp); //12
+
+      //Net force
+      vfx = _mm256_fmadd_ps(dx,d_3_over_2,vfx); //14
+      vfy = _mm256_fmadd_ps(dy,d_3_over_2,vfy); //16
+      vfz = _mm256_fmadd_ps(dz,d_3_over_2,vfz); //18
+    }
+      //
+      vxi = _mm256_fmadd_ps(vdt,vfx,vxi); //20
+      vyi = _mm256_fmadd_ps(vdt,vfy,vyi); //22
+      vzi = _mm256_fmadd_ps(vdt,vfz,vzi); //24
+       
+      _mm256_storeu_ps(&p->vx[i],vxi);
+      _mm256_storeu_ps(&p->vy[i],vyi);
+      _mm256_storeu_ps(&p->vz[i],vzi);
+  }
+  //3 floating-point operations
+  for (u64 i = 0; i < n; i+=8)
+    {
+      pxi = _mm256_load_ps(&p->x[i]);
+      pyi = _mm256_load_ps(&p->y[i]);
+      pzi = _mm256_load_ps(&p->z[i]);
+
+      vxi = _mm256_load_ps(&p->vx[i]);
+      vyi = _mm256_load_ps(&p->vy[i]);
+      vzi = _mm256_load_ps(&p->vz[i]);
+
+
+      vxi = _mm256_fmadd_ps(vdt,vxi,pxi);
+      vyi = _mm256_fmadd_ps(vdt,vyi,pyi);
+      vzi = _mm256_fmadd_ps(vdt,vzi,pzi);
+      _mm256_storeu_ps(&p->x[i],pxi);
+      _mm256_storeu_ps(&p->y[i],pyi);
+      _mm256_storeu_ps(&p->z[i],pzi); 
+    }
+}
+
+//
+int main(int argc, char **argv)
+{
+  //
+  const u64 n = (argc > 1) ? atoll(argv[1]) : 16384;
+  const u64 steps= 10;
+  const f32 dt = 0.01;
+
+  //
+  f64 rate = 0.0, drate = 0.0;
+
+  //Steps to skip for warm up
+  const u64 warmup = 3;
+  
+  //
+  particle_t *p = malloc(sizeof(particle_t));
+  p->x = aligned_alloc(ALIGN,sizeof(f32) * n);
+  p->y = aligned_alloc(ALIGN,sizeof(f32) * n);
+  p->z = aligned_alloc(ALIGN,sizeof(f32) * n);
+  p->vx = aligned_alloc(ALIGN,sizeof(f32) * n);
+  p->vy = aligned_alloc(ALIGN,sizeof(f32) * n);
+  p->vz = aligned_alloc(ALIGN,sizeof(f32) * n);
+
+  //
+  init(p, n);
+
+  const u64 s = sizeof(particle_t) * n;
+  
+  printf("\n\033[1mTotal memory size:\033[0m %llu B, %llu KiB, %llu MiB\n\n", s, s >> 10, s >> 20);
+  
+  //
+  printf("\033[1m%5s %10s %10s %8s\033[0m\n", "Step", "Time, s", "Interact/s", "GFLOP/s"); fflush(stdout);
+  
+  //
+  for (u64 i = 0; i < steps; i++)
+    {
+      //Measure
+      const f64 start = omp_get_wtime();
+
+      move_particles(p, dt, n);
+
+      const f64 end = omp_get_wtime();
+
+      //Number of interactions/iterations
+      const f32 h1 = (f32)(n) * (f32)(n - 1);
+
+      //GFLOPS
+      const f32 h2 = (24.0 * h1 + 3.0 * (f32)n) * 1e-9;
+      
+      if (i >= warmup)
+	{
+	  rate += h2 / (end - start);
+	  drate += (h2 * h2) / ((end - start) * (end - start));
+	}
+
+      //
+      printf("%5llu %10.3e %10.3e %8.1f %s\n",
+	     i,
+	     (end - start),
+	     h1 / (end - start),
+	     h2 / (end - start),
+	     (i < warmup) ? "*" : "");
+      
+      fflush(stdout);
+    }
+
+  //
+  rate /= (f64)(steps - warmup);
+  drate = sqrt(drate / (f64)(steps - warmup) - (rate * rate));
+
+  printf("-----------------------------------------------------\n");
+  printf("\033[1m%s %4s \033[42m%10.1lf +- %.1lf GFLOP/s\033[0m\n",
+	 "Average performance:", "", rate, drate);
+  printf("-----------------------------------------------------\n");
+  
+  //
+  free(p);
+
+  //
+  return 0;
+}
